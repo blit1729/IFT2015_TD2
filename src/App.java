1ba3ddbba476bde4701721299cbdc6b2df9882d3
blit1729 <125315624+blit1729@users.noreply.github.com>
2023-02-16 11:20:22 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // Vous pouvez tester votre code ici!
+        Node node1 = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		
+		System.out.println(node1.getValue());
+		System.out.println(node2.getValue());
+		System.out.println(node3.getValue());
+		
+		node1.addNode(node2);
+		
+		
+		System.out.println(node1.getNext().getValue());
     }
 }
